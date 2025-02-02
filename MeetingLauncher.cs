af18daeef72c760a7f830b48bf7be793d55ce693
Safir Kebieh <safeer-k@hotmail.com>
2025-02-02 06:59:22 +0100

    Initial commit

---
+++ MeetingLauncher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingManager
+{
+    public class MeetingLauncher
+    {   
+        public void GenerateMeetingLink()
+        {
+            // Generate a unique meeting name
+            string meetingName = "JitsiMeeting" + DateTime.Now.Ticks;
+            string userName = "Safir#ä"; // Change this to any name
+
+            // Encode the user name to handle special characters
+            string encodedUserName = Uri.EscapeDataString(userName);
+
+            // Generate the Jitsi meeting URL with user info
+            string meetingLink = $"https://meet.jit.si/{meetingName}#userInfo.displayName=\"{encodedUserName}\"";
+
+            Console.WriteLine($"📅 Meeting Created: {meetingLink}");
+
+            // Open the meeting in the default web browser
+            Process.Start(new ProcessStartInfo(meetingLink) { UseShellExecute = true });
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SpectraStyling.cs
@@ -0,0 +1,42 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MeetingManager.Properties;
+
+namespace MeetingManager
+{
+    public class SpectraStyling
+    {
+
+        public static void MainMenu()
+        {
+            Console.Title = "MeetingManager";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            AnsiConsole.Markup("[bold]Wellcome in our Program[/]\n\"");
+
+            // Create a list of choices
+            var options = new List<string>
+        {
+            "\U00002795 Start Meeting",
+            "\U00002795 Join Meeting",
+            "❌ Exit"
+        };
+
+            var selectedOption = AnsiConsole.Prompt(
+              new SelectionPrompt<string>()
+                  .Title("[bold]Use arrow keys to select an action:[/]")
+                  .PageSize(5)
+                  .AddChoices(options)
+                  .HighlightStyle(new Style(Color.Green))
+          );
+
+            AnsiConsole.Markup($"\n[bold green]✔ You selected: {selectedOption}[/]\n");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
