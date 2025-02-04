6a979c9f954d26cbbf4dd673af6f2181174486e2
Safir Kebieh <safeer-k@hotmail.com>
2025-02-04 13:00:06 +0100

    Create Strings and fill ListManageInvites

--- StringResources.cs
+++ StringResources.cs
@@ -12,9 +12,11 @@ namespace MeetingManager
             Exit
         });
 
-        public static readonly ReadOnlyCollection<string> ManageUsers = new ReadOnlyCollection<string>(new List<string>
+        public static readonly ReadOnlyCollection<string> ListManageInvites = new ReadOnlyCollection<string>(new List<string>
         {
-
+            InviteByEmail,
+            InviteFromDataBase,
+            CopyInviteLink
         });
 
         // List Menu
@@ -22,8 +24,12 @@ namespace MeetingManager
         public const string JoinRoom = "\U00002795 Join Room";
         public const string Exit = "❌ Exit";
 
+        // List ManageInvites
+        public const string InviteByEmail = "➕ Send an invitation directly via email to a specific recipient.";
+        public const string InviteFromDataBase = "➕ Select multiple recipients from your database and send group invitations.";
+        public const string CopyInviteLink = "➕ Copy the invitation link and share it manually at your convenience.";
 
-
+        //General
         public const string promptMessageRoomID = "[bold blue]Please Enter your Room ID:[/] ";
         public const string errorMessageRoomID = "[bold red]Invalid Room ID! It must be between 3 and 12 characters.[/]\n";
         public const string promtMessageUser = "[bold blue]Please Enter username:[/] ";
