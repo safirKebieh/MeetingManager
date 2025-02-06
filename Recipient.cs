70569e952a572e4af6986c9c48704cb054f9fc4f
Safir Kebieh <safeer-k@hotmail.com>
2025-02-06 19:46:39 +0100

    Add Class Recipient

---
+++ Recipient.cs
@@ -0,0 +1,35 @@
+﻿namespace MeetingManager
+{
+    public class Recipient
+    {
+        private string Name { get; set; }
+        private string Email { get; set; }
+
+        public Recipient(string name, string email) 
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            this.Name = name;
+        }
+
+        public string GetEmail()
+        {
+            return Email;
+        }
+
+        public void SetEmail(string email)
+        {
+            this.Email = email;
+        }
+
+    }
+}
