923320ed249de86101a7bc4d9d4945ea6d097d9d
Safir Kebieh <safeer-k@hotmail.com>
2025-02-06 19:56:20 +0100

    Modify class Recipient and Create Class Group

---
+++ Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeetingManager
+{
+    public class Group
+    {
+        public string Name { get; set; }
+        public List<Recipient> Members { get; set; }
+
+        public Group(string name, List<Recipient> recipients)
+        {
+            Name = name;
+            Members = recipients;
+        }
+    }
+}
--- Recipient.cs
+++ Recipient.cs
@@ -2,34 +2,13 @@
 {
     public class Recipient
     {
-        private string Name { get; set; }
-        private string Email { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
 
         public Recipient(string name, string email) 
         {
             Name = name;
             Email = email;
         }
-
-        public string GetName()
-        {
-            return Name;
-        }
-
-        public void SetName(string name)
-        {
-            this.Name = name;
-        }
-
-        public string GetEmail()
-        {
-            return Email;
-        }
-
-        public void SetEmail(string email)
-        {
-            this.Email = email;
-        }
-
     }
 }
